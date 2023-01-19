0c38e41444d5db62eb559632f50e8dc873a35bbc
Michele Sessa <mikysett@gmail.com>
2023-01-19 10:29:17 +0000

    test(scripting-piscine): improve py exercise tester
    switch from py2 to py3
    make pytest stop at first failure

--- sh/tests/entrypoint.sh
+++ sh/tests/entrypoint.sh
@@ -9,7 +9,7 @@ cd app
 if test -f "${EXERCISE}_test.sh"; then
 	bash "${EXERCISE}_test.sh"
 elif test -f "${EXERCISE}_test.py"; then
-	pytest "${EXERCISE}_test.py"
+	pytest -x "${EXERCISE}_test.py"
 else
 	echo "No test file found for the exercise : $EXERCISE"
 	exit 1
